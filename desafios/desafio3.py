ed5b51e50283417bb420912e8510da4bb95a3b52
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-13 17:03:14 -0300

    ordenando

--- desafios/desafio3.py
+++ desafios/desafio3.py
@@ -1,6 +1,12 @@
-n = (input('Digite algo: '))
-print(n.istitle())
-print(n.isspace())
-print(n.isupper())
-print(n.isdecimal())
 
+n = (input('Digite algo: '))
+print(n)
+print(type(n))
+
+n = str(input('Digite um valor: '))
+print(n)
+print(type(n))
+
+n = bool(input('Digite um valor: '))
+print(n)
+print(type(n))
--- desafios/desafio4.py
+++ desafios/desafio4.py
@@ -1,11 +1,5 @@
 n = (input('Digite algo: '))
-print(n)
-print(type(n))
-
-n = str(input('Digite um valor: '))
-print(n)
-print(type(n))
-
-n = bool(input('Digite um valor: '))
-print(n)
-print(type(n))
+print(n.istitle())
+print(n.isspace())
+print(n.isupper())
+print(n.isdecimal())
