d4624e54809067289295372f013be6b7e50af78d
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-17 14:50:37 -0300

    ex19

---
+++ desafios/desafio019.py
@@ -0,0 +1,12 @@
+import random
+
+aluno = input("digite o nome dos alunos que vao participar: ")
+alunos = aluno.split()
+escolhido = random.choice(alunos)
+print('O aluno sorteado para apagar a lousa é {}'.format(escolhido))
+
+
+
+pessoas = ['pedro', 'joao', 'victor', 'mathues']
+escolhido = random.choice(pessoas)
+print('O aluno sorteado para apagar a lousa é {}'.format(escolhido))
