0e3f1df7bf93ec11ab04ba89af83a6f8b64751a4
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-20 12:46:49 -0300

    ex 27

--- desafios/desafio027.py
+++ desafios/desafio027.py
@@ -1,6 +1,7 @@
 nome = input("Digite seu nome completo: ")
+print("O seu nome completo é {}".format(nome))
+nome = nome.title()
+nome = nome.split()
+print("O seu primeiro nome é: {}".format(nome[0]))
+print("O seu ultimo nome é: {}".format(nome[-1]))
 
-nome.split()
-
-print(nome[1])
-print(nome[])
