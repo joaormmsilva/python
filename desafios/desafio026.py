2e2ba353a165a6ffb60bae19751db0a3af869db9
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-20 12:39:23 -0300

    ex 26 e 27

---
+++ desafios/desafio026.py
@@ -0,0 +1,7 @@
+frase = input("digite uma frase qualquer: ")
+
+print(frase.lower().count('a',))
+
+print(frase.lower().find('a')+1)
+
+print(frase.lower().rfind('a')+1)
---
+++ desafios/desafio027.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome completo: ")
+
+nome.split()
+
+print(nome[1])
+print(nome[])
