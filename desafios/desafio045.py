daf9888cf7bc29f1f3131ad187b7fa309690cd55
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-22 12:46:46 -0300

    ex45

---
+++ desafios/desafio045.py
@@ -0,0 +1,15 @@
+import random 
+from time import sleep
+
+computador = random.choice(['pedra', 'papel', 'tesoura'])
+resposta = input('Escolha algum entre pedra, papel ou tesoura: ')
+
+print('Pensando...')
+sleep(2)
+
+if computador == 'pedra' and resposta == 'tesoura' or computador == 'tesoura' and resposta == 'papel' or computador == 'papel' and resposta == 'pedra':
+    print('\033[31mVoce perdeu para mim\033[m')
+    print('Eu escolhi {} e vc escolheu {}'.format(computador, resposta))
+else:
+    print('\033[32mVoce me venceu. Parabens\033[m')
+    print('Eu escolhi {} e vc escolheu {}'.format(computador, resposta))
