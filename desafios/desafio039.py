52bdd097d905b20adb465867380a4be58ac00c99
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-22 09:49:56 -0300

    ex39

---
+++ desafios/desafio039.py
@@ -0,0 +1,15 @@
+import datetime
+nascimento = int(input('Qual seu ano de nascimento: '))
+ano = datetime.date.today().year
+idade = ano - nascimento 
+faltam =  18 - idade 
+passou = faltam - faltam*2
+
+if idade <= 17:
+    print('\033[31mVocê ainda ira ter que fazer o alistamento militar\033[m')
+    print('faltam {} para voce passar pelo alistamento'.format(faltam))
+elif idade == 18:
+    print('Esta no ano de se alistar')
+elif idade > 18:
+    print('\033[32mVocê passou pelo alistamento militar\033[m')
+    print('fazem {} que voce passou pelo alistamento'.format(passou))
