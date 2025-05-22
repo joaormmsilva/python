457a6ff0e500174f4b95259d2b55dd6849b03dea
João Renato Silva <joaormmsilva@gmail.com>
2025-05-22 20:47:09 -0300

    ex004

---
+++ extra/ex004.py
@@ -0,0 +1,7 @@
+num1 = int(input('Digite um numero: '))
+num2 = int(input('Digite outra numero: '))
+
+if num1 > num2:
+    print('O {} é maior que o {}'.format(num1,num2))
+else:
+    print('O {} é maior que o {}'.format(num2,num1))
