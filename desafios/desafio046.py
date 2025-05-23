68d8456f496b44125c4f8ec92754553aabe8aad1
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-23 12:36:53 -0300

    ex46

---
+++ desafios/desafio046.py
@@ -0,0 +1,8 @@
+from time import sleep
+
+print("Contagem regresiva os fogos vão estourar em")
+for c in range(10,0-1, -1):
+    sleep(1)
+    print(c)
+sleep(1)
+print('BOMMMMMM')
---
+++ mundo2/aula002/ex001.py
@@ -0,0 +1,8 @@
+i = int(input('Digite um numero: '))
+f = int(input('Ate onde quer que el vá: '))
+p = int(input('de quantos em quantos: '))
+
+
+for c in range(i, f+1, p):
+    print(c)
+print('fim')
