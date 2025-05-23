7b7a82109cd81c17a095c43e7c23da73d9105c61
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-23 13:00:57 -0300

    ex54

--- desafios/desafio054.py
+++ desafios/desafio054.py
@@ -1,1 +1,7 @@
-frase = input("escreva uma frase: ")
+pesos=[]
+for c in range(5):
+    peso = int(input("fale seu peço "))
+    pesos.append(peso)
+ 
+pesos.sort()
+print('o maior peso é {} e o menor peso é {}'.format(pesos[0],pesos[-1]))
