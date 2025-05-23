1a75f643f33b8b9bdc09567307de3bae00e72d13
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-23 09:17:05 -0300

    Update desafio045.py

--- desafios/desafio045.py
+++ desafios/desafio045.py
@@ -10,6 +10,8 @@ sleep(2)
 if computador == 'pedra' and resposta == 'tesoura' or computador == 'tesoura' and resposta == 'papel' or computador == 'papel' and resposta == 'pedra':
     print('\033[31mVoce perdeu para mim\033[m')
     print('Eu escolhi {} e vc escolheu {}'.format(computador, resposta))
-else:
+elif resposta == 'pedra' and computador == 'tesoura' or resposta == 'tesoura' and computador == 'papel' or resposta == 'papel' and computador == 'pedra':
     print('\033[32mVoce me venceu. Parabens\033[m')
     print('Eu escolhi {} e vc escolheu {}'.format(computador, resposta))
+elif resposta == computador:
+    print('deu empate')
