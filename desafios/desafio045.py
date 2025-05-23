8a833448147dbb2bd541b1c5f845f4a93bb105de
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-23 09:20:15 -0300

    organização

--- desafios/desafio045.py
+++ desafios/desafio045.py
@@ -1,8 +1,10 @@
 import random 
 from time import sleep
+print('  ')
 
 computador = random.choice(['pedra', 'papel', 'tesoura'])
 resposta = input('Escolha algum entre pedra, papel ou tesoura: ')
+print('  ')
 
 print('Pensando...')
 sleep(2)
@@ -10,8 +12,13 @@ sleep(2)
 if computador == 'pedra' and resposta == 'tesoura' or computador == 'tesoura' and resposta == 'papel' or computador == 'papel' and resposta == 'pedra':
     print('\033[31mVoce perdeu para mim\033[m')
     print('Eu escolhi {} e vc escolheu {}'.format(computador, resposta))
+    print('  ')
+    
 elif resposta == 'pedra' and computador == 'tesoura' or resposta == 'tesoura' and computador == 'papel' or resposta == 'papel' and computador == 'pedra':
     print('\033[32mVoce me venceu. Parabens\033[m')
     print('Eu escolhi {} e vc escolheu {}'.format(computador, resposta))
+    print('  ')
+    
 elif resposta == computador:
     print('deu empate')
+    print('  ')
