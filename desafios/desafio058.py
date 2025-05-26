155c828ea407fabf1789ea99f503c3e1b137ec75
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-26 12:48:36 -0300

    correção

--- desafios/desafio058.py
+++ desafios/desafio058.py
@@ -3,13 +3,14 @@ numero = random.randint(0, 10)
 escolha = int(input('Adivinhe o numero que estou pensando: '))
 print('\nVoce errou')
 print('Eu escolhi o numero {} e você escolheu o numero {}'.format(numero, escolha))
-
+tentativas = 0
 
 while escolha != numero:
     numero = random.randint(0, 10)
     escolha = int(input('\nEscolha outro numero'))
     print('\nVocê errou\nEu escolhi o numero {} e você escolheu o numero {}'.format(numero, escolha))
+    tentativas += 1
 
         
 print('\nVocê Acertou o numero que eu escolhi foi {} e você colocou {}'.format(numero, escolha))
-print(' ')
+print('Voce precisou de {} tentativas para acertar'.format(tentativas))
