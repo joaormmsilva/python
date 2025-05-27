906bc3bd3785c5bb0c541257ed8b5a8561a7c3d3
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-27 13:03:37 -0300

    EX61

--- desafios/desafio061.py
+++ desafios/desafio061.py
@@ -1,10 +1,17 @@
 primeiro = int(input('Digite o primeiro termo da PA: '))
 razao = int(input('Digite a razão da PA: '))
+
 contador = 1
+total = 10
+termo = primeiro
 
-while contador <= 10:
-    print({primeiro})
-    primeiro += razao
-    contador += 1
+while total != 0:
+    while contador <= total:
+        print(f'{termo}', end=' → ')
+        termo += razao
+        contador += 1
+    print('PAUSA')
+    total = int(input('Quantos termos você quer mostrar a mais? (Digite 0 para sair): '))
+    contador = 1  # Resetar o contador para contar os novos termos
 
-print('FIM')
+print('FIM DA PA')
