926bf509738ec6722e76a0c09d00b44ad69e0d9d
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-27 12:38:06 -0300

    ex61

---
+++ desafios/desafio061.py
@@ -0,0 +1,10 @@
+primeiro = int(input('Digite o primeiro termo da PA: '))
+razao = int(input('Digite a razão da PA: '))
+contador = 1
+
+while contador <= 10:
+    print({primeiro})
+    primeiro += razao
+    contador += 1
+
+print('FIM')
