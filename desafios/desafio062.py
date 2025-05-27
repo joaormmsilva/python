be65781b3c283c0dc12b27bfc9164a789c88e02f
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-27 13:04:48 -0300

    ex62

---
+++ desafios/desafio062.py
@@ -0,0 +1,16 @@
+n = int(input('Quantos termos você quer mostrar? '))
+
+termo1 = 0
+termo2 = 1
+contador = 3
+
+print(f'{termo1} → {termo2}', end='')
+
+while contador <= n:
+    termo3 = termo1 + termo2
+    print(f' → {termo3}', end='')
+    termo1 = termo2
+    termo2 = termo3
+    contador += 1
+
+print(' → FIM')
