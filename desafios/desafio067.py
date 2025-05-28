6772f4f50ad3fc9118ea707ffaa0d4f953949bb4
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-28 12:20:06 -0300

    ex67

--- desafios/desafio067.py
+++ desafios/desafio067.py
@@ -1,13 +1,13 @@
-s = 0
-numeros = []
-cont = 0
-
-while True:
-    n = int(input('Digite um numero [para parar coloque 0] '))
-    if n == 0:
+multi = 0
+n = int(input("qual o valor quer a tabuada: "))
+while True: 
+    while multi <= 10:
+        res = n * multi
+        print(f'O valor de {n} x {multi} = {res}')
+        multi += 1
+    n = int(input('\nquer mais algum numero?\nSe quiser parar coleque 0: '))
+    if n != 0:
+        multi = 0
+    else:
         break
-    numeros.append(n)
-    s += n
-    cont += 1
-    
-print(f'Os numeros colados são {numeros} o tatal de numero foram {cont} e a soma de todos eles dara {s}')
+print('fim')
