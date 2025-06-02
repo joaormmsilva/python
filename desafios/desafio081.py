dcbf13bfd879b374ce5a1ab8df64ee964030df7e
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-06-02 12:39:06 -0300

    ex81

--- desafios/desafio081.py
+++ desafios/desafio081.py
@@ -1,7 +1,20 @@
-valores = []
-for c in range(5):
-    valor = int(input(f'digite o {c+1}° valor: '))
-    valores.append(valor)
-    valores.sort()
-    print(valores)
+numeros = []
+while True:
+    pos = 1
+    num = int(input(f'digite o {pos} valore'))
+    pos += 1
+    numeros.append(num)
+    verif = input('Quer contuniar [S/N]: ').lower().strip()
+    if verif == 'n':
+        break
+    
+print(f'A quantia de numeros digitador foi {len(numeros)}')
 
+numeros.sort(reverse=True)
+
+print(f'os numeros em ordem decrescente fica {numeros}')
+
+if 5 in numeros:
+    print('O valor 5 foi entrecontado na lista')
+else:
+    print('O valor 5 não foi encrontrado na lista')
