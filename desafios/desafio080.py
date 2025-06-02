9cda4b1eb66ce27539abe8f642237edc55b7383b
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-06-02 12:29:50 -0300

    .

--- desafios/desafio080.py
+++ desafios/desafio080.py
@@ -5,3 +5,20 @@ for c in range(5):
     valores.sort()
     print(valores)
 
+lista = []
+
+for i in range(5):
+    num = int(input(f"Digite o {i+1}º número: "))
+    if not lista or num > lista[-1]:
+        lista.append(num)
+        print("Adicionado ao final da lista.")
+    else:
+        pos = 0
+        while pos < len(lista):
+            if num <= lista[pos]:
+                lista.insert(pos, num)
+                print(f"Adicionado na posição {pos}.")
+                break
+            pos += 1
+
+print(f"\nOs valores ordenados são: {lista}")
