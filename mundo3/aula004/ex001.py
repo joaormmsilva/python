9aec4eb7777c696d5c19abeecf1b66ea92e8e683
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-06-06 10:37:53 -0300

    aula04

---
+++ mundo3/aula004/ex001.py
@@ -0,0 +1,27 @@
+# def soma(a, b):
+#     s = a + b
+#     print(f'a soma de {a} + {b} é {s}')
+    
+    
+# soma(10, 12)
+# soma(2, 20)
+# soma(15, 20)
+
+# def contador(*num):
+#     print(num)
+
+# contador(2,1,6)
+# contador(5,2,1,5)
+# contador(2,1,6,2,6)
+
+def dobra(lst):
+    pos = 0
+    while pos <len(lst):
+        lst[pos]*=2
+        pos+=1
+    print(lst)
+
+valor = [5,6,4,1,2,3,5]
+print(valor)
+dobra(valor)
+
