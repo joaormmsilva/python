1aa49cc0398e624b05bc3d6b63751ca66206e89f
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-06-07 09:26:36 -0300

    att ex98

--- desafios/desafio098.py
+++ desafios/desafio098.py
@@ -2,7 +2,7 @@ from time import sleep
 def contador(inicio,fim,passo):
     print(f'Contagem de {inicio} ate {fim} indo de {passo} em {passo}')
     if inicio < fim:
-        while inicio < fim+1:
+        while inicio <= fim:
             print(inicio, end=' ')
             inicio+=passo
     else:
@@ -11,6 +11,13 @@ def contador(inicio,fim,passo):
             inicio-=passo
     print('FIM!')
 
+
 contador(10,0,1)
 contador(0,10,1)
 
+
+inicio = int(input('Qual o inicio'))
+fim = int(input('Digite o final: '))
+passo = int(input('De quanto em quanto?'))
+
+contador(inicio, fim, passo)
