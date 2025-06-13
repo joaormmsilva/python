961def19e505ff5271d4e121ac0c9c07b8ec485d
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-06-13 12:52:44 -0300

    atts

---
+++ mundo3/aula006/ex001.py
@@ -0,0 +1,8 @@
+import uteis
+num = int(input('Digite um valor: '))
+fat = uteis.fatorial(num)
+tri = uteis.triplo(num)
+dobro = uteis.dobro(num)
+print(f'O fatorial de {num} é {fat}')
+print(f' o triplo de { num } é {tri}')
+print(f' o dobro de { num } é {dobro}')
