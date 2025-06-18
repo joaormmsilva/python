fcb10a2d44c5f4bf017145dc0aa2450690b681f7
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-06-18 09:14:47 -0300

    ex013/014

---
+++ extra/ex013.py
@@ -0,0 +1,4 @@
+num = input('digite um numero: ')
+numerico = float(num.replace(',','.'))
+print(type(numerico))
+print(numerico)
---
+++ extra/ex014.py
@@ -0,0 +1,8 @@
+from random import randint
+
+numeros = []
+
+for x in range(5):
+    numeros.append(randint(1,10))
+    
+print(numeros)
