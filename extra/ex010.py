b5d884d57fb037f1181ad188bcfe4b30fb36057a
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-06-18 08:39:32 -0300

    ex010

---
+++ extra/ex010.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+numero = int(input('digite um numero: '))
+
+dob = numero * 2
+tri = numero * 3
+raiz = sqrt(numero)
+
+print(f'A raiz de {numero} é {raiz}.\nO dobro de {numero} é {dob}\nO triplode {numero} é {tri}')
